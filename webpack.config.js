d3483da9bd734af88b4665f943d71068c2c1adb4
AlexandrKosov <alxkosov@yandex.ru>
2020-11-13 14:51:31 +0400

    непонятно что

--- webpack.config.js
+++ webpack.config.js
@@ -129,25 +129,25 @@ let conf = {
         historyApiFallback: true,
         overlay: true
     },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    name: `chunk-vendors`,
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                    chunks: 'initial'
-                },  
-                common: {
-                    name: `chunk-common`,
-                    minChunks: 2,
-                    priority: -20,
-                    chunks: 'initial',
-                    reuseExistingChunk: true
-                }
-            }
-        }
-    }
+    // optimization: {
+        // splitChunks: {
+            // cacheGroups: {
+                // vendors: {
+                    // name: `chunk-vendors`,
+                    // test: /[\\/]node_modules[\\/]/,
+                    // priority: -10,
+                    // chunks: 'initial'
+                // },  
+                // common: {
+                    // name: `chunk-common`,
+                    // minChunks: 2,
+                    // priority: -20,
+                    // chunks: 'initial',
+                    // reuseExistingChunk: true
+                // }
+            // }
+        // }
+    // }
 };
 
 module.exports = conf;
